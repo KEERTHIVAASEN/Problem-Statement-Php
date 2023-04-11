2b1517e2ba49cb679c98d561034e4bdf726c771e
KEERTHIVAASEN V <84181432+KEERTHIVAASEN@users.noreply.github.com>
2023-04-11 18:46:56 +0530

    UP

---
+++ Destroying Variables.php
@@ -0,0 +1,13 @@
+<?php
+
+	$car = 'Porsche'; // assign value to variable
+	unset($car);  // destroy variable
+?>
+
+
+Alternate Program
+
+<?php
+	$car = 'Porsche';
+	$car = null;  // assign empty value to variable
+?>
