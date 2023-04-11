5b071008c15e197e083128a4e92b0574029fe313
KEERTHIVAASEN V <84181432+KEERTHIVAASEN@users.noreply.github.com>
2023-04-11 17:50:41 +0530

    UP

---
+++ Assigning Values to Variables.php
@@ -0,0 +1,6 @@
+<?php
+	$now = 2008;  // assign value to variable
+	$currentYear = $now;  // assign variable to another variable
+	$lastYear = $currentYear - 1;  // perform calculation
+	echo "$lastYear has ended. Welcome to $currentYear!";
+?>
